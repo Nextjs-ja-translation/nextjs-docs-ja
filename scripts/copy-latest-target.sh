a4b319e7d72a8b65962bdb7da9d4cb8275102b5f
Hirofumi Wakasugi <baenej@gmail.com>
2018-12-06 09:48:24 +0900

    Change to retrieve the latest translation from github

--- scripts/copy-latest-target.sh
+++ scripts/copy-latest-target.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-src_path="${OMEGAT_PROJECT_PATH}/target/docs.mdx"
 dst_path="./components/docs/docs.mdx"
-cp $src_path $dst_path
+
+curl -o "${dst_path}" https://raw.githubusercontent.com/5t111111/nextjs-docs-ja-omegat/master/target/docs.mdx
