a36098cbe045a14db1c85f856247aeb518f99afe
Hirofumi Wakasugi <baenej@gmail.com>
2019-12-07 17:53:08 +0900

    Add translation information on top of the page

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,14 +1,52 @@
-import Page from '../../components/page';
-import Header from '../../components/header';
-import Navbar from '../../components/navbar';
-import Container from '../../components/container';
-import { MediaQueryConsumer } from '../../components/media-query';
-import withPure from '../../components/hoc/pure';
+import Page from "../../components/page";
+import Header from "../../components/header";
+import Navbar from "../../components/navbar";
+import Container from "../../components/container";
+import { MediaQueryConsumer } from "../../components/media-query";
+import withPure from "../../components/hoc/pure";
 
-import Markdown, { headings } from '../../components/docs/docs.mdx';
-import Documentation, { components } from '../../components/docs/documentation';
+import Markdown, { headings } from "../../components/docs/docs.mdx";
+import Documentation, { components } from "../../components/docs/documentation";
+import baseCommit from "../../base-commit.json";
 
 const Content = withPure(() => <Markdown components={components} />);
+
+const TranslationInformation = () => {
+  return (
+    <div>
+      <p>
+        このドキュメント日本語翻訳は、
+        <a
+          href="https://github.com/zeit/next-site"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ドキュメント原文の登録されているリポジトリ
+        </a>
+        のコミット <b>[{baseCommit.ref}]</b> を元に翻訳されています。
+      </p>
+      <p>
+        情報が古くなっている場合もありますので、できるだけ
+        <a
+          href="https://nextjs.org/docs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          原文のドキュメント
+        </a>
+        で最新の情報を参照ください。
+      </p>
+      <style jsx>{`
+        div {
+          background-color: #f7f7f7;
+          padding: 1.25rem;
+          margin: 40px 0;
+          border: 1px solid #d8d8d8;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default () => (
   <Page>
@@ -25,6 +63,7 @@ export default () => (
     </MediaQueryConsumer>
     <Container>
       <Documentation headings={headings}>
+        <TranslationInformation />
         <Content />
       </Documentation>
     </Container>
